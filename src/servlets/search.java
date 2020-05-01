c1a13971cda71bf5a1c25cf1e95f25bd11839b25
Vilina Ong <vilinaong@gmail.com>
2020-05-01 02:39:09 -0400

    fix clearing of text boxes

--- src/servlets/search.java
+++ src/servlets/search.java
@@ -89,6 +89,8 @@ public class search extends HttpServlet {
 					if (!date.isEmpty()) {
 						query += " departureDatetime like \"" + date + "%\""; 
 						session.setAttribute("da", date);
+					} else {
+						session.setAttribute("da", null);
 					}
 					
 					if (!sorigin.isEmpty()) {
@@ -98,6 +100,8 @@ public class search extends HttpServlet {
 							query += " origin = \"" + sorigin + "\"";
 						}
 						session.setAttribute("o", sorigin);
+					} else {
+						session.setAttribute("o", null);
 					}
 					
 					if (!sdestination.isEmpty()) {
@@ -107,6 +111,8 @@ public class search extends HttpServlet {
 							query += " destination = \"" + sdestination + "\"";						
 						}
 						session.setAttribute("de", sdestination);
+					} else {
+						session.setAttribute("de", null);
 					}
 				
 				}
